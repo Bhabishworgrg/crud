3d7293d06c00ad02b665249d8fc4f8b372dca96f
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-09-29 13:19:16 +0545

    Wrap post fetching in a function

--- src/script.js
+++ src/script.js
@@ -1,20 +1,26 @@
 const postsElement = document.getElementById('posts');
 
-const generatePosts = (data) => {
-	return data.map(post => `
-		<h2>${post.title}</h2>
-		<p>${post.body}</p>
-	`).join('');
+const fetchAllPosts = async () => {
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+		response.ok
+			? console.log('SUCCESS: Fetching all posts | Status', response.status)
+			: console.error('ERROR: Fetching all posts | Status', response.status);
+		const data = await response.json();
+		return data;
+	} catch(error) {
+		console.error('ERROR: Fetching all posts |', error);
+	}
 }
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-	.then(response => {
-		response.ok
-			? console.log('SUCCESS: Status', response.status)
-			: console.error('ERROR: Status', response.status);
-		return response.json();
-	})
-	.then(data => {
-		postsElement.innerHTML += generatePosts(data)
-	})
-	.catch(error => console.error('ERROR:', error));
+const generatePosts = async () => {
+	const data = await fetchAllPosts();
+	postsElement.innerHTML += data.map(post => `
+      <article class="post">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </article>
+    `).join('');
+}
+
+generatePosts();
