acd6fc7e054e91bc3cf3b8b463f8d30cd8c79d26
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-09-30 17:02:17 +0545

    Create create post page

---
+++ scripts/create.js
@@ -0,0 +1,31 @@
+const formElement = document.getElementById('create-post-form');
+const statusElement = document.getElementById('status');
+
+formElement.addEventListener('submit', async (event) => {
+	event.preventDefault();
+
+	const userId = formElement.userId.value.trim();
+	const title = formElement.title.value.trim();
+	const body = formElement.body.value.trim();
+
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ userId, title, body }),
+		});
+
+		if (!response.ok) console.error('ERROR: Creating post | Status', response.status);
+
+		const newPost = await response.json();
+		console.log('SUCCESS: Created post', newPost);
+		statusElement.textContent = `Post created with ID: ${newPost.id}`;
+
+		window.location.href = `post.html?id=${newPost.id}`;
+	} catch (error) {
+		console.error('ERROR: Creating post |', error);
+		statusElement.textContent = 'Failed to create post.';
+	}
+});
